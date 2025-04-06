6f8004e51c253bb65d4b92653abe8ce33903fdc7
tkatter <tjkatter@gmail.com>
2025-04-06 17:36:23 -0500

    setup placeholder components and played with css modules

---
+++ world-wise/src/pages/PageNotFound.jsx
@@ -0,0 +1,10 @@
+function PageNotFound() {
+  return (
+    <div>
+      <h1>404</h1>
+      <p style={{ textTransform: "uppercase" }}>page not found</p>
+    </div>
+  );
+}
+
+export default PageNotFound;
