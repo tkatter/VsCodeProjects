b07dcc02f342878decc49ebb3aad8d5f55b7526c
tkatter <tjkatter@gmail.com>
2025-04-06 20:22:37 -0500

    updated PageNav css for login button

--- world-wise/src/App.jsx
+++ world-wise/src/App.jsx
@@ -11,10 +11,10 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Homepage />} />
-        <Route path="app" element={<AppLayout />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
         <Route path="login" element={<Login />} />
+        <Route path="app" element={<AppLayout />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
