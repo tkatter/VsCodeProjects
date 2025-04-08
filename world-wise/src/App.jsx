c3db3a560d32a26ab053daa2115a127ed8e447a3
tkatter <tjkatter@gmail.com>
2025-04-07 20:52:20 -0500

    set up params with react router to pass data between pages

--- world-wise/src/App.jsx
+++ world-wise/src/App.jsx
@@ -1,5 +1,8 @@
+// Library modules
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useState, useEffect } from "react";
+
+// My modules
 import Product from "./pages/Product";
 import Homepage from "./pages/Homepage";
 import Pricing from "./pages/Pricing";
@@ -7,6 +10,7 @@ import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
 import CityList from "./components/CityList";
+import City from "./components/City";
 import CountryList from "./components/CountriesList";
 
 const BASE_URL = "http://localhost:8000";
@@ -47,6 +51,7 @@ function App() {
             path="cities"
             element={<CityList cities={cities} isLoading={isLoading} />}
           />
+          <Route path="cities/:id" element={<City />} />
           <Route
             path="countries"
             element={<CountryList cities={cities} isLoading={isLoading} />}
