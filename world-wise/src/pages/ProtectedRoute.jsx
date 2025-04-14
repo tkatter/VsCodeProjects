07354f5f69574235bdafe27952b2ae94221c6cb7
tkatter <tjkatter@gmail.com>
2025-04-13 20:22:33 -0500

    WorldWise application feature complete

---
+++ world-wise/src/pages/ProtectedRoute.jsx
@@ -0,0 +1,9 @@
+import { Navigate } from "react-router-dom";
+import { useAuth } from "../contexts/FakeAuthContext";
+
+function ProtectedRoute({ children }) {
+  const { isAuthenticated } = useAuth();
+  return isAuthenticated ? children : <Navigate to="/" />;
+}
+
+export default ProtectedRoute;
