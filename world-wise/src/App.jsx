7e9a6dd01e8d9b4bd011a1303b0f15e481c3634f
tkatter <tjkatter@gmail.com>
2025-04-14 20:55:26 -0500

    implemented lazy loading in WorldWise - finished project

--- world-wise/src/App.jsx
+++ world-wise/src/App.jsx
@@ -1,20 +1,58 @@
-// Library modules
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+// LIBRARY MODULES
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate,
+  useLocation,
+  Outlet,
+} from "react-router-dom";
+import { lazy, Suspense } from "react";
 
-// My modules
+// MY MODULES
+// CONTEXTS
+import { CitiesProvider } from "./contexts/CitiesContext";
+import { AuthProvider } from "./contexts/FakeAuthContext";
+
+// COMPONENTS
+import CityList from "./components/CityList";
+import City from "./components/City";
+import CountryList from "./components/CountryList";
+import Form from "./components/Form";
+import SpinnerFullPage from "./components/SpinnerFullPage";
+
+// PAGES
+import ProtectedRoute from "./pages/ProtectedRoute";
+
+/* pre-lazyloading
 import Product from "./pages/Product";
 import Homepage from "./pages/Homepage";
 import Pricing from "./pages/Pricing";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
-import CityList from "./components/CityList";
-import City from "./components/City";
-import CountryList from "./components/CountryList";
-import Form from "./components/Form";
-import { CitiesProvider } from "./contexts/CitiesContext";
-import { AuthProvider } from "./contexts/FakeAuthContext";
-import ProtectedRoute from "./pages/ProtectedRoute";
+
+dist/index.html                   0.45 kB │ gzip:   0.29 kB
+dist/assets/index-uciEGT7U.css   30.32 kB │ gzip:   5.06 kB
+dist/assets/index-Cx6pAdf0.js   569.10 kB │ gzip: 168.93 kB
+
+*/
+
+const Homepage = lazy(() => import("./pages/Homepage"));
+const Product = lazy(() => import("./pages/Product"));
+const Pricing = lazy(() => import("./pages/Pricing"));
+const PageNotFound = lazy(() => import("./pages/PageNotFound"));
+const AppLayout = lazy(() => import("./pages/AppLayout"));
+const Login = lazy(() => import("./pages/Login"));
+
+function Layout() {
+  let location = useLocation();
+  return (
+    <Suspense fallback={<SpinnerFullPage />} key={location.key}>
+      <Outlet />
+    </Suspense>
+  );
+}
 
 function App() {
   return (
@@ -22,23 +60,25 @@ function App() {
       <CitiesProvider>
         <BrowserRouter>
           <Routes>
-            <Route index element={<Homepage />} />
-            <Route path="product" element={<Product />} />
-            <Route path="pricing" element={<Pricing />} />
-            <Route path="login" element={<Login />} />
-            <Route
-              path="app"
-              element={
-                <ProtectedRoute>
-                  <AppLayout />
-                </ProtectedRoute>
-              }
-            >
-              <Route index element={<Navigate replace to="cities" />} />
-              <Route path="cities" element={<CityList />} />
-              <Route path="cities/:id" element={<City />} />
-              <Route path="countries" element={<CountryList />} />
-              <Route path="form" element={<Form />} />
+            <Route path="/" element={<Layout />}>
+              <Route index element={<Homepage />} />
+              <Route path="product" element={<Product />} />
+              <Route path="pricing" element={<Pricing />} />
+              <Route path="login" element={<Login />} />
+              <Route
+                path="app"
+                element={
+                  <ProtectedRoute>
+                    <AppLayout />
+                  </ProtectedRoute>
+                }
+              >
+                <Route index element={<Navigate replace to="cities" />} />
+                <Route path="cities" element={<CityList />} />
+                <Route path="cities/:id" element={<City />} />
+                <Route path="countries" element={<CountryList />} />
+                <Route path="form" element={<Form />} />
+              </Route>
             </Route>
             <Route path="*" element={<PageNotFound />} />
           </Routes>
